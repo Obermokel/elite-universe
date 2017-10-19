825292ad1d7440b14ac98c1568eb6d716ac44aca
Boris Günther <boris.guenther@googlemail.com>
2017-10-19 22:53:55 +0200

    Use T410s for ES

--- src/main/java/borg/ed/universe/UniverseApplication.java
+++ src/main/java/borg/ed/universe/UniverseApplication.java
@@ -1,9 +1,7 @@
 package borg.ed.universe;
 
-import borg.ed.universe.eddn.EddnElasticUpdater;
-import borg.ed.universe.eddn.EddnReaderThread;
-import borg.ed.universe.journal.JournalEventReader;
-import borg.ed.universe.journal.JournalReaderThread;
+import java.net.InetSocketAddress;
+
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -21,62 +19,65 @@ import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-import java.net.InetSocketAddress;
+import borg.ed.universe.eddn.EddnElasticUpdater;
+import borg.ed.universe.eddn.EddnReaderThread;
+import borg.ed.universe.journal.JournalEventReader;
+import borg.ed.universe.journal.JournalReaderThread;
 
 @SpringBootApplication
 @EnableElasticsearchRepositories(basePackages = "borg.ed.universe.repository")
 @ComponentScan(basePackages = { "borg.ed.universe.service.impl", "borg.ed.universe.converter" })
 public class UniverseApplication {
 
-    static final Logger logger = LoggerFactory.getLogger(UniverseApplication.class);
+	static final Logger logger = LoggerFactory.getLogger(UniverseApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(UniverseApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UniverseApplication.class, args);
+	}
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            EddnReaderThread eddnReaderThread = ctx.getBean(EddnReaderThread.class);
-            eddnReaderThread.addListener(ctx.getBean(EddnElasticUpdater.class));
-            eddnReaderThread.start();
-        };
-    }
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			EddnReaderThread eddnReaderThread = ctx.getBean(EddnReaderThread.class);
+			eddnReaderThread.addListener(ctx.getBean(EddnElasticUpdater.class));
+			eddnReaderThread.start();
+		};
+	}
 
-    @Bean
-    public JournalEventReader journalEventReader() {
-        return new JournalEventReader();
-    }
+	@Bean
+	public JournalEventReader journalEventReader() {
+		return new JournalEventReader();
+	}
 
-    @Bean
-    public JournalReaderThread journalReaderThread() {
-        return new JournalReaderThread();
-    }
+	@Bean
+	public JournalReaderThread journalReaderThread() {
+		return new JournalReaderThread();
+	}
 
-    @Bean
-    public EddnReaderThread eddnReaderThread() {
-        return new EddnReaderThread();
-    }
+	@Bean
+	public EddnReaderThread eddnReaderThread() {
+		return new EddnReaderThread();
+	}
 
-    @Bean
-    public EddnElasticUpdater eddnElasticUpdater() {
-        return new EddnElasticUpdater();
-    }
+	@Bean
+	public EddnElasticUpdater eddnElasticUpdater() {
+		return new EddnElasticUpdater();
+	}
 
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchTemplate(this.client());
-    }
+	@Bean
+	public ElasticsearchOperations elasticsearchTemplate() {
+		return new ElasticsearchTemplate(this.client());
+	}
 
-    @Bean
-    public Client client() {
-        Settings settings = Settings.builder().put("cluster.name", "eddbmirror").build();
-        TransportClient client = new PreBuiltTransportClient(settings);
-        client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9300)));
-        return client;
-        //		TransportClient client = TransportClient.builder().settings(settings).build();
-        //		client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.31", 9300)));
-        //		return client;
-    }
+	@Bean
+	public Client client() {
+		Settings settings = Settings.builder().put("cluster.name", "eddbmirror").build();
+		TransportClient client = new PreBuiltTransportClient(settings);
+		client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.3", 9300))); // 192.168.178.3 = T410s
+		return client;
+		//		TransportClient client = TransportClient.builder().settings(settings).build();
+		//		client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.31", 9300)));
+		//		return client;
+	}
 
 }
