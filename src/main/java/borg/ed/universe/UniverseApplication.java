b1b44e71e497ed8482e175bedfe86f38f478f1cf
Boris Günther <boris.guenther@googlemail.com>
2017-11-01 00:30:43 +0100

    Use XPC

--- src/main/java/borg/ed/universe/UniverseApplication.java
+++ src/main/java/borg/ed/universe/UniverseApplication.java
@@ -73,8 +73,9 @@ public class UniverseApplication {
 	public Client client() {
 		Settings settings = Settings.builder().put("cluster.name", "eddbmirror").build();
 		TransportClient client = new PreBuiltTransportClient(settings);
-		client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.3", 9300))); // 192.168.178.3 = T410s
 		//client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9300)));
+		//client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.3", 9300))); // 192.168.178.3 = T410s
+		client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.23", 9300))); // 192.168.178.23 = XPC
 		return client;
 		//		TransportClient client = TransportClient.builder().settings(settings).build();
 		//		client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.178.31", 9300)));
