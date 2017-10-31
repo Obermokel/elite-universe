6097804861c40a68933319c342555ec204a0d2a8
Boris Günther <boris.guenther@googlemail.com>
2017-10-31 19:17:29 +0100

    EDDB planet classes

--- src/main/java/borg/ed/universe/constants/PlanetClass.java
+++ src/main/java/borg/ed/universe/constants/PlanetClass.java
@@ -9,7 +9,23 @@ import org.apache.commons.lang.StringUtils;
  */
 public enum PlanetClass {
 
-	AMMONIA_WORLD, EARTHLIKE_BODY, GAS_GIANT_WITH_AMMONIA_BASED_LIFE, GAS_GIANT_WITH_WATER_BASED_LIFE, HELIUM_RICH_GAS_GIANT, HIGH_METAL_CONTENT_BODY, ICY_BODY, METAL_RICH_BODY, ROCKY_BODY, ROCKY_ICY_BODY, SUDARSKY_CLASS_I_GAS_GIANT, SUDARSKY_CLASS_II_GAS_GIANT, SUDARSKY_CLASS_III_GAS_GIANT, SUDARSKY_CLASS_IV_GAS_GIANT, SUDARSKY_CLASS_V_GAS_GIANT, WATER_GIANT, WATER_WORLD;
+	AMMONIA_WORLD,
+	EARTHLIKE_BODY,
+	GAS_GIANT_WITH_AMMONIA_BASED_LIFE,
+	GAS_GIANT_WITH_WATER_BASED_LIFE,
+	HELIUM_RICH_GAS_GIANT,
+	HIGH_METAL_CONTENT_BODY,
+	ICY_BODY,
+	METAL_RICH_BODY,
+	ROCKY_BODY,
+	ROCKY_ICY_BODY,
+	SUDARSKY_CLASS_I_GAS_GIANT,
+	SUDARSKY_CLASS_II_GAS_GIANT,
+	SUDARSKY_CLASS_III_GAS_GIANT,
+	SUDARSKY_CLASS_IV_GAS_GIANT,
+	SUDARSKY_CLASS_V_GAS_GIANT,
+	WATER_GIANT,
+	WATER_WORLD;
 
 	public static PlanetClass fromJournalValue(String value) {
 		if (StringUtils.isEmpty(value)) {
@@ -19,32 +35,44 @@ public enum PlanetClass {
 			case "Ammonia world":
 				return PlanetClass.AMMONIA_WORLD;
 			case "Earthlike body":
+			case "Earth-like world":
 				return PlanetClass.EARTHLIKE_BODY;
 			case "Gas giant with ammonia based life":
+			case "Gas giant with ammonia-based life":
 				return PlanetClass.GAS_GIANT_WITH_AMMONIA_BASED_LIFE;
 			case "Gas giant with water based life":
+			case "Gas giant with water-based life":
 				return PlanetClass.GAS_GIANT_WITH_WATER_BASED_LIFE;
 			case "Helium rich gas giant":
+			case "Helium-rich gas giant":
 				return PlanetClass.HELIUM_RICH_GAS_GIANT;
 			case "High metal content body":
+			case "High metal content world":
 				return PlanetClass.HIGH_METAL_CONTENT_BODY;
 			case "Icy body":
 				return PlanetClass.ICY_BODY;
 			case "Metal rich body":
+			case "Metal-rich body":
 				return PlanetClass.METAL_RICH_BODY;
 			case "Rocky body":
 				return PlanetClass.ROCKY_BODY;
 			case "Rocky ice body":
+			case "Rocky ice world":
 				return PlanetClass.ROCKY_ICY_BODY;
 			case "Sudarsky class I gas giant":
+			case "Class I gas giant":
 				return PlanetClass.SUDARSKY_CLASS_I_GAS_GIANT;
 			case "Sudarsky class II gas giant":
+			case "Class II gas giant":
 				return PlanetClass.SUDARSKY_CLASS_II_GAS_GIANT;
 			case "Sudarsky class III gas giant":
+			case "Class III gas giant":
 				return PlanetClass.SUDARSKY_CLASS_III_GAS_GIANT;
 			case "Sudarsky class IV gas giant":
+			case "Class IV gas giant":
 				return PlanetClass.SUDARSKY_CLASS_IV_GAS_GIANT;
 			case "Sudarsky class V gas giant":
+			case "Class V gas giant":
 				return PlanetClass.SUDARSKY_CLASS_V_GAS_GIANT;
 			case "Water giant":
 				return PlanetClass.WATER_GIANT;
