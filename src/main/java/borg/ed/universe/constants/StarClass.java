e18e3c77d2fee1d8e86bdf5c16e96848297a8730
Boris Günther <boris.guenther@redteclab.com>
2018-04-24 17:59:37 +0200

    Utility methods

--- src/main/java/borg/ed/universe/constants/StarClass.java
+++ src/main/java/borg/ed/universe/constants/StarClass.java
@@ -9,143 +9,153 @@ import org.apache.commons.lang.StringUtils;
  */
 public enum StarClass {
 
-	O,
-	B,
-	A,
-	F,
-	G,
-	K,
-	M,
-	//A_BLUE_WHITE_SUPER_GIANT,
-	AEBE,
-	C,
-	CJ,
-	CN,
-	D,
-	DA,
-	DAB,
-	DAV,
-	DAZ,
-	DB,
-	DBV,
-	DBZ,
-	DC,
-	DCV,
-	DQ,
-	DX,
-	//F_WHITE_SUPER_GIANT,
-	H,
-	//K_ORANGE_GIANT,
-	L,
-	//M_RED_GIANT,
-	//M_RED_SUPER_GIANT,
-	MS,
-	N,
-	//SUPER_MASSIVE_BLACK_HOLE,
-	S,
-	T,
-	TTS,
-	W,
-	WC,
-	WN,
-	WNC,
-	WO,
-	Y;
+    O,
+    B,
+    A,
+    F,
+    G,
+    K,
+    M,
+    //A_BLUE_WHITE_SUPER_GIANT,
+    AEBE,
+    C,
+    CJ,
+    CN,
+    D,
+    DA,
+    DAB,
+    DAV,
+    DAZ,
+    DB,
+    DBV,
+    DBZ,
+    DC,
+    DCV,
+    DQ,
+    DX,
+    //F_WHITE_SUPER_GIANT,
+    H,
+    //K_ORANGE_GIANT,
+    L,
+    //M_RED_GIANT,
+    //M_RED_SUPER_GIANT,
+    MS,
+    N,
+    //SUPER_MASSIVE_BLACK_HOLE,
+    S,
+    T,
+    TTS,
+    W,
+    WC,
+    WN,
+    WNC,
+    WO,
+    Y;
 
-	public static StarClass fromJournalValue(String value) {
-		if (StringUtils.isEmpty(value)) {
-			return null;
-		} else {
-			switch (value) {
-			case "O":
-				return StarClass.O;
-			case "B":
-				return StarClass.B;
-			case "A":
-				return StarClass.A;
-			case "F":
-				return StarClass.F;
-			case "G":
-				return StarClass.G;
-			case "K":
-				return StarClass.K;
-			case "M":
-				return StarClass.M;
-			case "A_BlueWhiteSuperGiant":
-				return StarClass.A;
-			case "AeBe":
-			case "AEBE":
-				return StarClass.AEBE;
-			case "C":
-				return StarClass.C;
-			case "CJ":
-				return StarClass.CJ;
-			case "CN":
-				return StarClass.CN;
-			case "D":
-				return StarClass.D;
-			case "DA":
-				return StarClass.DA;
-			case "DAB":
-				return StarClass.DAB;
-			case "DAV":
-				return StarClass.DAV;
-			case "DAZ":
-				return StarClass.DAZ;
-			case "DB":
-				return StarClass.DB;
-			case "DBV":
-				return StarClass.DBV;
-			case "DBZ":
-				return StarClass.DBZ;
-			case "DC":
-				return StarClass.DC;
-			case "DCV":
-				return StarClass.DCV;
-			case "DQ":
-				return StarClass.DQ;
-			case "DX":
-				return StarClass.DX;
-			case "F_WhiteSuperGiant":
-				return StarClass.F;
-			case "H":
-				return StarClass.H;
-			case "K_OrangeGiant":
-				return StarClass.K;
-			case "L":
-				return StarClass.L;
-			case "M_RedGiant":
-				return StarClass.M;
-			case "M_RedSuperGiant":
-				return StarClass.M;
-			case "MS":
-				return StarClass.MS;
-			case "N":
-				return StarClass.N;
-			case "S":
-				return StarClass.S;
-			case "SupermassiveBlackHole":
-				return StarClass.H;
-			case "T":
-				return StarClass.T;
-			case "TTS":
-				return StarClass.TTS;
-			case "W":
-				return StarClass.W;
-			case "WC":
-				return StarClass.WC;
-			case "WN":
-				return StarClass.WN;
-			case "WNC":
-				return StarClass.WNC;
-			case "WO":
-				return StarClass.WO;
-			case "Y":
-				return StarClass.Y;
-			default:
-				throw new IllegalArgumentException("Unknown value '" + value + "' for StarClass");
-			}
-		}
-	}
+    private final String shortName;
+
+    private StarClass() {
+        this.shortName = this.name();
+    }
+
+    public static StarClass fromJournalValue(String value) {
+        if (StringUtils.isEmpty(value)) {
+            return null;
+        } else {
+            switch (value) {
+                case "O":
+                    return StarClass.O;
+                case "B":
+                    return StarClass.B;
+                case "A":
+                    return StarClass.A;
+                case "F":
+                    return StarClass.F;
+                case "G":
+                    return StarClass.G;
+                case "K":
+                    return StarClass.K;
+                case "M":
+                    return StarClass.M;
+                case "A_BlueWhiteSuperGiant":
+                    return StarClass.A;
+                case "AeBe":
+                case "AEBE":
+                    return StarClass.AEBE;
+                case "C":
+                    return StarClass.C;
+                case "CJ":
+                    return StarClass.CJ;
+                case "CN":
+                    return StarClass.CN;
+                case "D":
+                    return StarClass.D;
+                case "DA":
+                    return StarClass.DA;
+                case "DAB":
+                    return StarClass.DAB;
+                case "DAV":
+                    return StarClass.DAV;
+                case "DAZ":
+                    return StarClass.DAZ;
+                case "DB":
+                    return StarClass.DB;
+                case "DBV":
+                    return StarClass.DBV;
+                case "DBZ":
+                    return StarClass.DBZ;
+                case "DC":
+                    return StarClass.DC;
+                case "DCV":
+                    return StarClass.DCV;
+                case "DQ":
+                    return StarClass.DQ;
+                case "DX":
+                    return StarClass.DX;
+                case "F_WhiteSuperGiant":
+                    return StarClass.F;
+                case "H":
+                    return StarClass.H;
+                case "K_OrangeGiant":
+                    return StarClass.K;
+                case "L":
+                    return StarClass.L;
+                case "M_RedGiant":
+                    return StarClass.M;
+                case "M_RedSuperGiant":
+                    return StarClass.M;
+                case "MS":
+                    return StarClass.MS;
+                case "N":
+                    return StarClass.N;
+                case "S":
+                    return StarClass.S;
+                case "SupermassiveBlackHole":
+                    return StarClass.H;
+                case "T":
+                    return StarClass.T;
+                case "TTS":
+                    return StarClass.TTS;
+                case "W":
+                    return StarClass.W;
+                case "WC":
+                    return StarClass.WC;
+                case "WN":
+                    return StarClass.WN;
+                case "WNC":
+                    return StarClass.WNC;
+                case "WO":
+                    return StarClass.WO;
+                case "Y":
+                    return StarClass.Y;
+                default:
+                    throw new IllegalArgumentException("Unknown value '" + value + "' for StarClass");
+            }
+        }
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
 
 }
--- src/main/java/borg/ed/universe/util/BodyUtil.java
+++ src/main/java/borg/ed/universe/util/BodyUtil.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import borg.ed.universe.constants.PlanetClass;
 import borg.ed.universe.constants.StarClass;
 import borg.ed.universe.constants.TerraformingState;
+import borg.ed.universe.journal.events.ScanEvent;
 import borg.ed.universe.model.Body;
 
 /**
@@ -15,54 +16,84 @@ import borg.ed.universe.model.Body;
  */
 public abstract class BodyUtil {
 
-	static final Logger logger = LoggerFactory.getLogger(BodyUtil.class);
+    static final Logger logger = LoggerFactory.getLogger(BodyUtil.class);
 
-	public static long estimatePayout(Body body) {
-		return BodyUtil.estimatePayout(body.getStarClass(), body.getPlanetClass(), TerraformingState.TERRAFORMABLE.equals(body.getTerraformingState()));
-	}
+    public static String getAbbreviatedType(Body b) {
+        return BodyUtil.getAbbreviatedType(b.getStarClass(), b.getPlanetClass(), TerraformingState.TERRAFORMABLE.equals(b.getTerraformingState()));
+    }
 
-	public static long estimatePayout(StarClass starClass, PlanetClass planetClass, boolean terraformingCandidate) {
-		if (starClass != null) {
-			switch (starClass) {
-			case H:
-				return 60000;
-			case N:
-				return 55000;
-			case D:
-			case DA:
-			case DAB:
-			case DAV:
-			case DAZ:
-			case DB:
-			case DC:
-			case DCV:
-			case DQ:
-				return 35000;
-			default:
-				return 3000;
-			}
-		} else if (planetClass != null) {
-			switch (planetClass) {
-			case EARTHLIKE_BODY:
-				return 650000;
-			case WATER_WORLD:
-				return terraformingCandidate ? 600000 : 250000;
-			case HIGH_METAL_CONTENT_BODY:
-				return terraformingCandidate ? 375000 : 34000;
-			case ROCKY_BODY:
-				return terraformingCandidate ? 180000 : 1000;
-			case AMMONIA_WORLD:
-				return 320000;
-			case METAL_RICH_BODY:
-				return 65000;
-			case SUDARSKY_CLASS_II_GAS_GIANT:
-				return 53000;
-			default:
-				return 500;
-			}
-		} else {
-			return 0;
-		}
-	}
+    public static String getAbbreviatedType(ScanEvent e) {
+        StarClass starClass = StarClass.fromJournalValue(e.getStarType());
+        PlanetClass planetClass = PlanetClass.fromJournalValue(e.getPlanetClass());
+        TerraformingState terraformingState = TerraformingState.fromJournalValue(e.getTerraformState());
+
+        return BodyUtil.getAbbreviatedType(starClass, planetClass, TerraformingState.TERRAFORMABLE.equals(terraformingState));
+    }
+
+    public static String getAbbreviatedType(StarClass starClass, PlanetClass planetClass, boolean terraformingCandidate) {
+        if (starClass != null) {
+            return starClass.getShortName();
+        } else if (planetClass != null) {
+            return planetClass.getShortName() + (terraformingCandidate ? "-TF" : "");
+        } else {
+            return "UNKNOWN";
+        }
+    }
+
+    public static long estimatePayout(Body b) {
+        return BodyUtil.estimatePayout(b.getStarClass(), b.getPlanetClass(), TerraformingState.TERRAFORMABLE.equals(b.getTerraformingState()));
+    }
+
+    public static long estimatePayout(ScanEvent e) {
+        StarClass starClass = StarClass.fromJournalValue(e.getStarType());
+        PlanetClass planetClass = PlanetClass.fromJournalValue(e.getPlanetClass());
+        TerraformingState terraformingState = TerraformingState.fromJournalValue(e.getTerraformState());
+
+        return BodyUtil.estimatePayout(starClass, planetClass, TerraformingState.TERRAFORMABLE.equals(terraformingState));
+    }
+
+    public static long estimatePayout(StarClass starClass, PlanetClass planetClass, boolean terraformingCandidate) {
+        if (starClass != null) {
+            switch (starClass) {
+                case H:
+                    return 60000;
+                case N:
+                    return 55000;
+                case D:
+                case DA:
+                case DAB:
+                case DAV:
+                case DAZ:
+                case DB:
+                case DC:
+                case DCV:
+                case DQ:
+                    return 35000;
+                default:
+                    return 3000;
+            }
+        } else if (planetClass != null) {
+            switch (planetClass) {
+                case EARTHLIKE_BODY:
+                    return 650000;
+                case WATER_WORLD:
+                    return terraformingCandidate ? 600000 : 250000;
+                case HIGH_METAL_CONTENT_BODY:
+                    return terraformingCandidate ? 375000 : 34000;
+                case ROCKY_BODY:
+                    return terraformingCandidate ? 180000 : 1000;
+                case AMMONIA_WORLD:
+                    return 320000;
+                case METAL_RICH_BODY:
+                    return 65000;
+                case SUDARSKY_CLASS_II_GAS_GIANT:
+                    return 53000;
+                default:
+                    return 500;
+            }
+        } else {
+            return 0;
+        }
+    }
 
 }
