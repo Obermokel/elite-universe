bd6e54f778232c29ad1ee4c7a56adac5f43a2fb2
Boris Günther <boris.guenther@googlemail.com>
2018-05-07 09:34:42 +0200

    Ignore Powerplay event

--- src/main/java/borg/ed/universe/journal/JournalEventReader.java
+++ src/main/java/borg/ed/universe/journal/JournalEventReader.java
@@ -43,90 +43,90 @@ import borg.ed.universe.util.GsonZonedDateTime;
  */
 public class JournalEventReader {
 
-    static final Logger logger = LoggerFactory.getLogger(JournalEventReader.class);
+	static final Logger logger = LoggerFactory.getLogger(JournalEventReader.class);
 
-    private final Gson gson = new GsonBuilder().registerTypeAdapter(ZonedDateTime.class, new GsonZonedDateTime())
-            .registerTypeAdapter(Coord.class, new GsonCoord()).create();
+	private final Gson gson = new GsonBuilder().registerTypeAdapter(ZonedDateTime.class, new GsonZonedDateTime()).registerTypeAdapter(Coord.class, new GsonCoord()).create();
 
-    @SuppressWarnings("deprecation")
-    public AbstractJournalEvent readLine(String line) {
-        if (StringUtils.isBlank(line)) {
-            return null;
-        } else {
-            try {
-                LinkedHashMap<String, Object> data = this.gson.fromJson(line, LinkedHashMap.class);
-                String event = (String) data.get("event");
+	@SuppressWarnings("deprecation")
+	public AbstractJournalEvent readLine(String line) {
+		if (StringUtils.isBlank(line)) {
+			return null;
+		} else {
+			try {
+				LinkedHashMap<String, Object> data = this.gson.fromJson(line, LinkedHashMap.class);
+				String event = (String) data.get("event");
 
-                switch (event) {
-                    case "Location":
-                        return this.gson.fromJson(line, LocationEvent.class);
-                    case "StartJump":
-                        return this.gson.fromJson(line, StartJumpEvent.class);
-                    case "FSDJump":
-                        return this.gson.fromJson(line, FSDJumpEvent.class);
-                    case "DiscoveryScan":
-                        return this.gson.fromJson(line, DiscoveryScanEvent.class);
-                    case "Scan":
-                        try {
-                            return this.gson.fromJson(line, ScanEvent.class);
-                        } catch (JsonSyntaxException e) {
-                            return this.gson.fromJson(line, ScanEventOld.class).toNewScanEvent();
-                        }
-                    case "FuelScoop":
-                        return this.gson.fromJson(line, FuelScoopEvent.class);
-                    case "SupercruiseEntry":
-                        return this.gson.fromJson(line, SupercruiseEntryEvent.class);
-                    case "SupercruiseExit":
-                        return this.gson.fromJson(line, SupercruiseExitEvent.class);
-                    case "Docked":
-                        return this.gson.fromJson(line, DockedEvent.class);
-                    case "Undocked":
-                        return this.gson.fromJson(line, UndockedEvent.class);
-                    case "LoadGame":
-                        return this.gson.fromJson(line, LoadGameEvent.class);
-                    case "Loadout":
-                        return this.gson.fromJson(line, LoadoutEvent.class);
-                    case "Cargo":
-                        return this.gson.fromJson(line, CargoEvent.class);
-                    case "Materials":
-                        return this.gson.fromJson(line, MaterialsEvent.class);
-                    case "SellExplorationData":
-                        return this.gson.fromJson(line, SellExplorationDataEvent.class);
-                    case "Died":
-                        return this.gson.fromJson(line, DiedEvent.class);
-                    case "ReceiveText":
-                        return this.gson.fromJson(line, ReceiveTextEvent.class);
-                    case "SendText":
-                        return this.gson.fromJson(line, SendTextEvent.class);
-                    case "Music":
-                        return this.gson.fromJson(line, MusicEvent.class);
-                    case "Scanned":
-                    case "CommitCrime":
-                    case "Bounty":
-                    case "RedeemVoucher":
-                    case "Screenshot":
-                    case "Friends":
-                    case "Fileheader":
-                    case "Progress":
-                    case "Rank":
-                    case "ShieldState":
-                    case "HullDamage":
-                    case "DockingRequested":
-                    case "DockingGranted":
-                    case "BuyAmmo":
-                    case "Repair":
-                    case "RepairAll":
-                    case "RestockVehicle":
-                        return null; // Irrelevant
-                    default:
-                        logger.debug("Unknown event '" + event + "'");
-                        return null;
-                }
-            } catch (JsonSyntaxException e) {
-                logger.error("Failed to read journal event\n\t" + line + "\n\t" + e);
-                return null;
-            }
-        }
-    }
+				switch (event) {
+				case "Location":
+					return this.gson.fromJson(line, LocationEvent.class);
+				case "StartJump":
+					return this.gson.fromJson(line, StartJumpEvent.class);
+				case "FSDJump":
+					return this.gson.fromJson(line, FSDJumpEvent.class);
+				case "DiscoveryScan":
+					return this.gson.fromJson(line, DiscoveryScanEvent.class);
+				case "Scan":
+					try {
+						return this.gson.fromJson(line, ScanEvent.class);
+					} catch (JsonSyntaxException e) {
+						return this.gson.fromJson(line, ScanEventOld.class).toNewScanEvent();
+					}
+				case "FuelScoop":
+					return this.gson.fromJson(line, FuelScoopEvent.class);
+				case "SupercruiseEntry":
+					return this.gson.fromJson(line, SupercruiseEntryEvent.class);
+				case "SupercruiseExit":
+					return this.gson.fromJson(line, SupercruiseExitEvent.class);
+				case "Docked":
+					return this.gson.fromJson(line, DockedEvent.class);
+				case "Undocked":
+					return this.gson.fromJson(line, UndockedEvent.class);
+				case "LoadGame":
+					return this.gson.fromJson(line, LoadGameEvent.class);
+				case "Loadout":
+					return this.gson.fromJson(line, LoadoutEvent.class);
+				case "Cargo":
+					return this.gson.fromJson(line, CargoEvent.class);
+				case "Materials":
+					return this.gson.fromJson(line, MaterialsEvent.class);
+				case "SellExplorationData":
+					return this.gson.fromJson(line, SellExplorationDataEvent.class);
+				case "Died":
+					return this.gson.fromJson(line, DiedEvent.class);
+				case "ReceiveText":
+					return this.gson.fromJson(line, ReceiveTextEvent.class);
+				case "SendText":
+					return this.gson.fromJson(line, SendTextEvent.class);
+				case "Music":
+					return this.gson.fromJson(line, MusicEvent.class);
+				case "Scanned":
+				case "CommitCrime":
+				case "Bounty":
+				case "RedeemVoucher":
+				case "Screenshot":
+				case "Friends":
+				case "Fileheader":
+				case "Powerplay":
+				case "Progress":
+				case "Rank":
+				case "ShieldState":
+				case "HullDamage":
+				case "DockingRequested":
+				case "DockingGranted":
+				case "BuyAmmo":
+				case "Repair":
+				case "RepairAll":
+				case "RestockVehicle":
+					return null; // Irrelevant
+				default:
+					logger.debug("Unknown event '" + event + "'");
+					return null;
+				}
+			} catch (JsonSyntaxException e) {
+				logger.error("Failed to read journal event\n\t" + line + "\n\t" + e);
+				return null;
+			}
+		}
+	}
 
 }
